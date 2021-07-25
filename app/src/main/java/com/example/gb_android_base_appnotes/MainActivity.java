36ccc5ff5a75c4800ecb6c156d4c93102a72b510
Ilya Vorobyev <vorbinn@list.ru>
2021-07-25 17:08:20 +0300

    Приложение откорректировано, для отображения экранов используется подход Single Activity

--- app/src/main/java/com/example/gb_android_base_appnotes/MainActivity.java
+++ app/src/main/java/com/example/gb_android_base_appnotes/MainActivity.java
@@ -1,16 +1,52 @@
 package com.example.gb_android_base_appnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ManageFragment {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            initViewFirst();
+        } else {
+            initView();
+        }
     }
 
+    private void initViewFirst() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, new TitleFragment());
+        fragmentTransaction.commit();
+    }
 
+    private void initView() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new TitleFragment());
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void replaceFragment(Note currentNote) {
+        NoteFragment detail = NoteFragment.newInstance(currentNote);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, detail);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
 }
--- app/src/main/java/com/example/gb_android_base_appnotes/TitleFragment.java
+++ app/src/main/java/com/example/gb_android_base_appnotes/TitleFragment.java
@@ -1,7 +1,7 @@
 package com.example.gb_android_base_appnotes;
 
 import android.content.Context;
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.graphics.Paint;
 import android.os.Bundle;
@@ -19,10 +19,13 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class TitleFragment extends Fragment {
+    public static final String SHARED_PREFERENCE_NAME = "SaveSelection";
+    public static final String SHARED_PREFERENCE_INDEX = "IndexNote";
     public static final String CURRENT_NOTE = "CurrentNote";
+    public static int INDEX_NOTE;
     private Note currentNote;
     private boolean isLandscape;
-
+    private ManageFragment manFrag;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -62,7 +65,9 @@ public class TitleFragment extends Fragment {
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
 
+        manFrag =  (ManageFragment) requireActivity();
         isLandscape = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+
     }
 
     @Override
@@ -74,6 +79,7 @@ public class TitleFragment extends Fragment {
     @Override
     public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
         super.onViewStateRestored(savedInstanceState);
+
         if (savedInstanceState != null) {
             currentNote = savedInstanceState.getParcelable(CURRENT_NOTE);
         } else {
@@ -82,6 +88,8 @@ public class TitleFragment extends Fragment {
                     getResources().getStringArray(R.array.date)[0],
                     getResources().getStringArray(R.array.description)[0]);
         }
+
+        getSelectionNote();
 
         if (isLandscape){
             showLandNote(currentNote);
@@ -99,20 +107,43 @@ public class TitleFragment extends Fragment {
     private void showLandNote(Note currentNote) {
         NoteFragment detail = NoteFragment.newInstance(currentNote);
 
+        saveSelection(currentNote);
+
         FragmentManager fragmentManager = requireActivity()
                 .getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.note, detail);
+        fragmentTransaction.replace(R.id.fragment_note, detail);
         fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
         fragmentTransaction.commit();
-
     }
 
     private void showPortNote(Note currentNote) {
-        Intent intent = new Intent();
-        intent.setClass(getActivity(), NoteActivity.class);
-        intent.putExtra(NoteFragment.ARG_NOTE, currentNote);
-        startActivity(intent);
+        saveSelection(currentNote);
+        manFrag.replaceFragment(currentNote);
+    }
+
+    private void saveSelection(Note currentNote) {
+        SharedPreferences sharedPref = requireActivity()
+                .getSharedPreferences(SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(SHARED_PREFERENCE_INDEX, currentNote.getIndexNote());
+        editor.commit();
+    }
+
+    private void getSelectionNote() {
+        SharedPreferences sharedPref = requireActivity()
+                .getSharedPreferences(SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE);
+
+        INDEX_NOTE = sharedPref.getInt(SHARED_PREFERENCE_INDEX, currentNote.getIndexNote());
+
+        if (INDEX_NOTE >= 0) {
+            currentNote = new Note (INDEX_NOTE,
+                    getResources().getStringArray(R.array.titles)[INDEX_NOTE],
+                    getResources().getStringArray(R.array.date)[INDEX_NOTE],
+                    getResources().getStringArray(R.array.description)[INDEX_NOTE]);
+        }
+
     }
 
 }
